b5bb39863b68272837d968fe7b1608fbfb970c20
fx <MacPro@CC5484GOHKL.itestaff.ite.edu.sg>
2019-07-16 15:14:44 +0800

    Added ENv

---
+++ Assets/_Environment/BG_Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_Move : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 16 * Time.deltaTime);
+
+        if (transform.position.z <= -8)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 45);
+    }
+}
