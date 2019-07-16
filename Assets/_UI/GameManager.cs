eac43abab099d3c15afb8cc63ab8fe8441e93b14
fx <MacPro@CC5484GOHKL.itestaff.ite.edu.sg>
2019-07-16 15:26:22 +0800

    Added UI Resources

---
+++ Assets/_UI/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState {  GameOver, GameStart, GameIdle};
+    public static GameState CurrentState = GameState.GameIdle;
+
+    public static int Lives = 3;
+    public static int Score = 0;
+
+    void Start()
+    {
+        Lives = 3;
+        Score = 0;
+        Time.timeScale = 0;
+        CurrentState = GameState.GameIdle;
+    }
+    
+    void Update()
+    {
+        if(CurrentState == GameState.GameIdle && Input.GetKeyDown(KeyCode.Return))
+        {            
+            CurrentState = GameState.GameStart;
+            Time.timeScale = 1;
+            HUD.HUDManager.DismissMessage();
+        }
+
+        else if(CurrentState == GameState.GameOver && Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/_UI/HUD.cs
@@ -0,0 +1,41 @@
+﻿﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public static HUD HUDManager;
+    [SerializeField] private Text Txt_Score = null;
+    [SerializeField] private Image Image_Lives = null;
+    [SerializeField] private Text Txt_Message = null;
+ 
+    void Start()
+    {
+        HUDManager = this;
+    }
+
+    public void UpdateScore()
+    {
+        Txt_Score.text = "SCORE : " + GameManager.Score;
+    }
+
+    //updates the number of hearts for lives
+    public void UpdateLives()
+    {
+        Image_Lives.rectTransform.sizeDelta = new Vector2(GameManager.Lives * 50, 30);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        GameManager.CurrentState = GameManager.GameState.GameOver;
+        Txt_Message.color = Color.red;
+        Txt_Message.text = "GAME OVER! \n PRESS ENTER TO RESTART GAME.";
+    }
+
+    public void DismissMessage()
+    {
+        Txt_Message.text = "";
+    }
+}
