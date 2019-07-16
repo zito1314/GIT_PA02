6e19106ea07a0440a8a5dae287f4dbf36cfbcf35
fx <MacPro@CC5484GOHKL.itestaff.ite.edu.sg>
2019-07-16 15:19:40 +0800

    Added Obstacles Resources

---
+++ Assets/_Obstacles/Obstacles.cs
@@ -0,0 +1,25 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    [SerializeField] private int Hitpoints = 3;
+    [SerializeField] private bool RandomRotation = false;
+
+    private void Start()
+    {
+        if(RandomRotation)
+            transform.eulerAngles = new Vector3(Random.Range(0, 180), Random.Range(0, 180), Random.Range(0, 180));
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - ( 8 * 2 * Time.deltaTime));
+
+        if(transform.position.z <= -8)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
